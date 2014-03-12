edbedce844be962444f9814d5f1ee4ace40b0fc2
Jason Kriss <jasonkriss@gmail.com>
2014-03-11 17:36:27 -0700

    remove deploy hooks

--- deploy/after_restart.rb
+++
@@ -1,7 +0,0 @@
-node[:deploy].each do |application, deploy|
-  if deploy[:application_type] == 'rails'
-    process_name = "#{application}_sidekiq"
-    Chef::Log.info("Restarting #{process_name} in after_restart")
-    sudo "monit restart all -g #{process_name}"
-  end
-end
