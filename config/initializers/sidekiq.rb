1ef416fc44993851e866daba503bb413f229bf72
Jason Kriss <jasonkriss@gmail.com>
2014-03-12 21:40:10 -0700

    fix sidekiq namespace

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,7 @@ config = YAML::load_file(Rails.root.join('config', 'redis.yml'))
 
 redis_opts = {
   url: "redis://#{config['host']}:#{config['port']}/0",
-  namespace: 'monospace.sidekiq'
+  namespace: 'opsworks.example.sidekiq'
 }
 
 Sidekiq.configure_server { |c| c.redis = redis_opts }
